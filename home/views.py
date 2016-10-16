f77ea4ed7fff828a4181e23d089fca93c2d8bb44
yatharth <=>
2016-10-17 00:49:19 +0530

    X-Frame Allow Fix

--- home/views.py
+++ home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 # Create your views here.
+
+@xframe_options_exempt
 def index(request):
    return render(request, "home/template/index.html", {})
